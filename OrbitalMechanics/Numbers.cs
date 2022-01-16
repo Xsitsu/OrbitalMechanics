37adf64af89601f8c1faf2498f1de1e4c5a9d12e
Xsitsu <jacoblocke21@live.com>
2022-01-15 23:55:35 -0800

    Add support for elliptical orbits using the 6 Keplerian elements. Function to calculate true anomaly is not implemented yet.

--- OrbitalMechanics/Numbers.cs
+++ OrbitalMechanics/Numbers.cs
@@ -6,17 +6,26 @@ namespace OrbitalMechanics
 {
     public static class Numbers
     {
+        // Constants
         public static double G = 6.673 * System.Math.Pow(10, -11);                  // (N*m)^2 / kg^2
         public static double C = 299792458;                                         // m/s
+        public static double PI2 = System.Math.PI * 2;
+        public static double PISquared = System.Math.PI * System.Math.PI;
+
+        // Time
         public static double YearToSeconds = 60 * 60 * 24 * 365.25;
         public static double DayToSeconds = 60 * 60 * 24;
         public static double HourToSecond = 60 * 60;
         public static double MinuteToSecond = 60;
+
+        // Distance
         public static double LightYearToKM = 9460730472580.8;
         public static double AUToKM = 149597870;
+        public static double KMToM = 1000;
+
+        // Mass
         public static double SolarMassToKG = 1.989 * System.Math.Pow(10, 30);
         public static double EarthMassToKGs = 5.972 * System.Math.Pow(10, 24);
-        public static double PI2 = System.Math.PI * 2;
-        public static double PISquared = System.Math.PI * System.Math.PI;
+
     }
 }
--- OrbitalMechanics/Orbital/OrbitalBase.cs
+++ OrbitalMechanics/Orbital/OrbitalBase.cs
@@ -9,12 +9,15 @@ namespace OrbitalMechanics.Orbital
         public OrbitalBase parent;                                          // The object that this object orbits around
         public List<OrbitalBase> satellites = new List<OrbitalBase>();      // List of satellites orbiting around this object
 
-        public double MassKg;                                               // KiloGrams
-        public double OrbitRadiusM;                                         // Meters
-        public double OrbitInclinationDeg;                                  // Degrees relative to parent's equator
+        public double MassKG;                                               // KiloGrams
+
+        public double Eccentricity;                                         // Unitless
+        public double SemiMajorAxisKM;                                      // KiloMeters
+        public double InclinationDeg;                                       // Degrees
         public double LongitudeOfANDeg;                                     // Longitude of Ascending Node in degrees
-        public double AxialTiltDeg;                                         // Degrees relative to orbital plane
-        public double RotationalPeriodSec;                                  // Seconds sidereal
+        public double ArgumentOfPeriapsisDeg;                               // Degrees
+        public double TimeOfPeriapsis;                                      // Seconds 
+
 
         public void AddSatellite(OrbitalBase satellite)
         {
@@ -54,17 +57,32 @@ namespace OrbitalMechanics.Orbital
 
 
 
+        public double CalculatePeriapsisKM()
+        {
+            return SemiMajorAxisKM * (1 - Eccentricity);
+        }
+        public double CalculateApoapsisKM()
+        {
+            return SemiMajorAxisKM * (1 + Eccentricity);
+        }
 
+
+        // Force that this object would exert on another at given distance. (m / s^2)
+        public double CalculateGravityFromDistance(double distanceM)
+        {
+            return Numbers.G * (MassKG / (distanceM * distanceM));
+        }
         // Orbital period in seconds of the time it takes for this
         // object to complete one orbit around it's parent.
-        public double CalculateOrbitalPeriod()
+        public double CalculateOrbitalPeriodSec()
         {
             if (parent != null)
             {
-                double rad3 = OrbitRadiusM * OrbitRadiusM * OrbitRadiusM;
+                double SemiMajorAxisM = SemiMajorAxisKM * Numbers.KMToM;
+                double SMA3 = SemiMajorAxisM * SemiMajorAxisM * SemiMajorAxisM;
 
-                double top = (4 * Numbers.PISquared * rad3);
-                double bottom = (Numbers.G * parent.MassKg);
+                double top = (4 * Numbers.PISquared * SMA3);
+                double bottom = (Numbers.G * parent.MassKG);
 
                 double period = System.Math.Sqrt(top / bottom);
 
@@ -72,10 +90,10 @@ namespace OrbitalMechanics.Orbital
             }
             return 0;
         }
-        // Force that this object would exert on another at given distance. (m / s^2)
-        public double CalculateGravityFromDistance(double distanceM)
+        // TODO: Implement method
+        public double CalculateMeanAnomalyDeg(double atTimeSec)
         {
-            return Numbers.G * (MassKg / (distanceM * distanceM));
+            return 0;
         }
     }
 }
