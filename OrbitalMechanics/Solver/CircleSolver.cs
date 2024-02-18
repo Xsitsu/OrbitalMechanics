e08b3602da2a44613a1f2364addffc8042ba754f
Xsitsu <jacoblocke21@live.com>
2024-02-17 16:22:13 -0800

    Move CircleSolver to correct namespace

--- OrbitalMechanics/Solver/CircleSolver.cs
+++ OrbitalMechanics/Solver/CircleSolver.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Numerics;
 using OrbitalMechanics.Solver;
 
-namespace OrbitalMechanics
+namespace OrbitalMechanics.Solver
 {
     public class CircleSolver : ISolver
     {
